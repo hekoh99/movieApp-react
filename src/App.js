bbd0f501d657ca7890e36ab3874b915d8e5f195d
hekoh99 <hekoh99@naver.com>
2020-07-27 22:48:44 +0900

    learn component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-function App() {
-  return <div>hello world!</div>;
+function Movie(i) { //매개변수 i는 객체. 
+  return <h3>my favorite director : {i.fav}</h3>;
 }
+
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Movie fav="chanwookPark" /> 
+      <Movie fav="junhoBong" />
+    </div>
+    );
+}//필요한 변수를 객체에 담아 컴포넌트로 전달 (fav)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <App />, document.getElementById('root') //App 컴포넌트가 반환하는 값을 index.html의 root 엘리먼트에 그린다
 );
 
 
